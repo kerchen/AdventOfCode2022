66c64f8cdcbd438d6a71aafd50fe37c1009b0912
pk <paul@whirlingchair.com>
2022-12-01 15:47:44 -0500

    Initial setup and day 1 completed.

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,48 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var max_calories = 0
+        var sum_calories = 0
+        var elf = 0
+
+        for (calories in input) {
+            val c: Int = try { calories.toInt() } catch (e:NumberFormatException) { 0 }
+            if (c != 0) {
+                sum_calories += c
+            } else {
+                if (sum_calories > max_calories) {
+                    max_calories = sum_calories
+                }
+                sum_calories = 0
+                elf += 1
+            }
+        }
+
+        return max_calories
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val elf_calories = mutableListOf<Int>()
+        var elf = 0
+
+        elf_calories.add(0)
+
+        for (calories in input) {
+            val c: Int = try { calories.toInt() } catch (e:NumberFormatException) { 0 }
+            if (c != 0) {
+                elf_calories[elf] += c
+            } else {
+                elf_calories.add(0)
+                elf += 1
+            }
+        }
+
+        val sorted_elf_calories = elf_calories.sortedDescending()
+        return sorted_elf_calories[0] + sorted_elf_calories[1] + sorted_elf_calories[2]
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    //val testInput = readInput("Day01_test")
+    //check(part1(testInput) == 1)
 
     val input = readInput("Day01")
     println(part1(input))
