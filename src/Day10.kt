2995482a40a7de112b585589052420e086b219dc
pk <paul@whirlingchair.com>
2022-12-10 19:38:53 -0500

    Day 10 completed

--- src/Day10.kt
+++ src/Day10.kt
@@ -35,15 +35,59 @@ fun main() {
         return signalStrengthSum
     }
 
-    fun part2(input: List<String>): Int {
-        return 0
+    fun part2(input: List<String>): List<CharArray> {
+        val columnCount = 40
+        val rowCount = 6
+        val image = List(rowCount) { CharArray(columnCount) { '.' } }
+        var x = 1
+        var counter = 0
+        var cycle = 0
+        var imageRow = 0
+        var imageColumn = 0
+        var instruction: Instruction
+
+        for (instructionString in input) {
+            counter += 1
+            instruction = if (instructionString.startsWith("noop")) {
+                Instruction(1, 0)
+            } else {
+                Instruction(2, instructionString.split(" ")[1].toInt())
+            }
+            for (crtCycle in IntRange(cycle, cycle+instruction.duration-1)) {
+                if (imageColumn >= x - 1 && imageColumn <= x + 1) {
+                    image[imageRow][imageColumn] = '#'
+                }
+                imageColumn += 1
+                if (imageColumn == columnCount)
+                {
+                    imageColumn = 0
+                    imageRow += 1
+                }
+            }
+            x += instruction.delta
+            cycle += instruction.duration
+        }
+        return image
     }
 
     val testInput = readInput("Day10_test")
     check(part1(testInput) == 13140)
-    check(part2(testInput) == 0)
+    val testImage = part2(testInput)
+    val expectedImage = listOf(
+        "##..##..##..##..##..##..##..##..##..##..",
+        "###...###...###...###...###...###...###.",
+        "####....####....####....####....####....",
+        "#####.....#####.....#####.....#####.....",
+        "######......######......######......####",
+        "#######.......#######.......#######.....")
+    for (row in IntRange(0, 5)) {
+        check(String(testImage[row]) == expectedImage[row])
+    }
 
     val input = readInput("Day10")
     println(part1(input))
-    println(part2(input))
+    val image = part2(input)
+    for (row in image) {
+        println(row)
+    }
 }
