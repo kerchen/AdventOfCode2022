8a162b2903aa6a425f0879a5f2e9695cf8cf9555
pk <paul@whirlingchair.com>
2022-12-14 19:48:31 -0500

    Day 13 part 1 completed

---
+++ src/Day13.kt
@@ -0,0 +1,166 @@
+
+
+fun findClosingBracket(input: String, start: Int): Int {
+    var i = start
+    var nestLevel = 1
+
+    while (nestLevel != 0) {
+        when(input[i]) {
+            '[' -> nestLevel += 1
+            ']' -> nestLevel -= 1
+        }
+        i += 1
+    }
+    return i
+}
+
+class Day13 {
+    abstract class SequenceItem {
+        abstract fun isAtomic(): Boolean
+        abstract fun convertToList(): SequenceList
+    }
+
+    class SequenceNumber(val number: Int) : SequenceItem() {
+        override fun isAtomic(): Boolean {
+            return true
+        }
+
+        override fun convertToList(): SequenceList {
+            return SequenceList("$number")
+        }
+    }
+
+    class SequenceList(input: String) : SequenceItem() {
+        var items = mutableListOf<SequenceItem>()
+
+        override fun isAtomic(): Boolean {
+            return false
+        }
+
+        override fun convertToList(): SequenceList {
+            return this
+        }
+
+        init {
+            var start = 0
+
+            while ( start < input.length ) {
+                when(input[start]) {
+                    '[' -> {
+                        var end = findClosingBracket(input, start+1)
+                        items.add(SequenceList(input.substring(start+1, end-1)))
+                        start = end
+                    }
+                    ',' -> {
+                        start += 1
+                    }
+                    else -> {
+                        var number = 0
+                        while(start < input.length && input[start].isDigit()) {
+                            number = number * 10 + input[start].digitToInt()
+                            start += 1
+                        }
+                        items.add(SequenceNumber(number))
+                    }
+                }
+            }
+        }
+    }
+
+    class Packet(input: String) {
+        val sequence = SequenceList(input)
+
+    }
+}
+
+enum class Ordered {
+    PUNT,
+    YES,
+    NO
+}
+
+fun isWellOrdered(leftList: Day13.SequenceList, rightList: Day13.SequenceList): Ordered
+{
+    val leftIterator = leftList.items.iterator()
+    val rightIterator = rightList.items.iterator()
+
+    while (leftIterator.hasNext() && rightIterator.hasNext())
+    {
+        val leftVal = leftIterator.next()
+        val rightVal = rightIterator.next()
+
+        if (leftVal.isAtomic() && rightVal.isAtomic()) {
+            val leftNumber = (leftVal as Day13.SequenceNumber).number
+            val rightNumber = (rightVal as Day13.SequenceNumber).number
+            if (leftNumber < rightNumber) return Ordered.YES
+            if (leftNumber > rightNumber) return Ordered.NO
+        } else if (!leftVal.isAtomic() && !rightVal.isAtomic()) {
+            val subResult = isWellOrdered(leftVal.convertToList(), rightVal.convertToList())
+            if (subResult != Ordered.PUNT)
+                return subResult
+        } else if (!leftVal.isAtomic()) {
+            val subResult = isWellOrdered(leftVal as Day13.SequenceList, rightVal.convertToList())
+            if (subResult != Ordered.PUNT)
+                return subResult
+        } else {
+            val subResult = isWellOrdered(leftVal.convertToList(), rightVal.convertToList())
+            if (subResult != Ordered.PUNT)
+                return subResult
+        }
+    }
+
+    if (!leftIterator.hasNext()) {
+        if (rightIterator.hasNext()) {
+            return Ordered.YES
+        }
+    }
+    if (!rightIterator.hasNext()) {
+        if (leftIterator.hasNext()) {
+            return Ordered.NO
+        }
+    }
+    return Ordered.PUNT
+}
+
+fun isWellOrdered(leftSide: Day13.Packet, rightSide: Day13.Packet): Boolean {
+    return isWellOrdered(leftSide.sequence, rightSide.sequence) == Ordered.YES
+}
+
+fun main() {
+    fun part1(input: List<String>): Int {
+        val inputIterator = input.iterator()
+        var packetIndex = 1
+        val wellOrderedPackets = mutableSetOf<Int>()
+
+        while(inputIterator.hasNext()) {
+            val leftString = inputIterator.next()
+            var leftPacket = Day13.Packet(leftString.substring(1, leftString.length-1 ))
+            val rightString = inputIterator.next()
+            var rightPacket = Day13.Packet(rightString.substring(1, rightString.length-1))
+
+            if (isWellOrdered(leftPacket, rightPacket)) {
+                wellOrderedPackets.add(packetIndex)
+            }
+            packetIndex += 1
+
+            // Skip blank separator line if present
+            if (inputIterator.hasNext()) {
+                inputIterator.next()
+            }
+        }
+        println(wellOrderedPackets.sum())
+        return wellOrderedPackets.sum()
+    }
+
+    fun part2(input: List<String>): Int {
+        return 0
+    }
+
+    val testInput = readInput("Day13_test")
+    check(part1(testInput) == 13)
+    check(part2(testInput) == 0)
+
+    val input = readInput("Day13")
+    println(part1(input))
+    println(part2(input))
+}
